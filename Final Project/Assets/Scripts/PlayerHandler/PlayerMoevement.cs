9d6b949fee2da88c8c37a88ecc30f548c9a59adb
YOSI <YOSI>
2024-10-30 21:48:02 +0200

    sync2

--- Final Project/Assets/Scripts/PlayerHandler/PlayerMoevement.cs
+++ Final Project/Assets/Scripts/PlayerHandler/PlayerMoevement.cs
@@ -1,19 +1,21 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    Rigidbody2D rb;
+    private Animator animator;
+    private Rigidbody2D rb;
+
     [SerializeField] float speed = 10f;
     [SerializeField] public float jumpAmount = 10f;
 
-    private bool isGrounded = true;  // Starts as true to allow initial jump
+    private bool isGrounded = true;
     private Vector2 direction;
 
     void Start()
     {
+        animator = GetComponent<Animator>();
         rb = GetComponent<Rigidbody2D>();
+
         if (rb == null)
             Debug.LogError("Rigidbody2D could not be found");
     }
@@ -23,7 +25,6 @@ public class PlayerMovement : MonoBehaviour
         float horizontalInput = Input.GetAxis("Horizontal");
         direction = new Vector2(horizontalInput, 0);
 
-        // Allow jump when space is pressed and player is grounded
         if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
         {
             Jump();
@@ -35,21 +36,20 @@ public class PlayerMovement : MonoBehaviour
         rb.velocity = new Vector2(direction.x * speed, rb.velocity.y);
     }
 
-    void Jump()
+    private void Jump()
     {
+        JumpAnimation();
         rb.AddForce(Vector2.up * jumpAmount, ForceMode2D.Impulse);
-        isGrounded = false;  // Once the player jumps, they're no longer grounded
+        isGrounded = false;
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        // When colliding with any surface, set isGrounded to true
-        if (collision.contacts[0].normal.y > 0.5f)  // Checks if the collision is from below
+        if (collision.contacts[0].normal.y > 0.5f)
         {
             isGrounded = true;
         }
 
-        // Handle ball collision to trigger jump
         if (collision.gameObject.CompareTag(ObjectTagsEnum.Ball.ToString()))
         {
             Jump();
@@ -58,10 +58,19 @@ public class PlayerMovement : MonoBehaviour
 
     private void OnCollisionExit2D(Collision2D collision)
     {
-        // Check if there are any contacts before accessing them
         if (collision.contactCount > 0 && collision.contacts[0].normal.y > 0.5f)
         {
             isGrounded = false;
         }
+    }
+
+    private void JumpAnimation()
+    {
+        animator.SetBool("IsJumping", true);
+    }
+
+    private void WalkAnimation()
+    {
+
     }
 }
