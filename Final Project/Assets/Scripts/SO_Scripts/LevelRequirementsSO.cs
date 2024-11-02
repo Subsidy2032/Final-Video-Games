7b7ae3badf61ba19b33ddb6cb2b3abe2ad09f03a
Subsidy2032 <noam133m@gmail.com>
2024-11-02 23:41:16 +0200

    some buttons not working

--- Final Project/Assets/Scripts/SO_Scripts/LevelRequirementsSO.cs
+++ Final Project/Assets/Scripts/SO_Scripts/LevelRequirementsSO.cs
@@ -6,8 +6,8 @@ public class LevelRequirementsSO : ScriptableObject
 {
     public Dictionary<string, int> requiredPoints = new Dictionary<string, int>
     {
-        { SceneNamesEnum.Level1.ToString(), 100 },
-        { SceneNamesEnum.Level2.ToString(), 0 },
-        { SceneNamesEnum.Level3.ToString(), 0 }
+        { SceneNamesEnum.Level1.ToString(), 150 },
+        { SceneNamesEnum.Level2.ToString(), 100 },
+        { SceneNamesEnum.Level3.ToString(), 100 }
     };
 }
